353980e52e37f95894106391c8a5b70f20ec60f9
Duy Thai <duythai2303@gmail.com>
2025-08-12 18:27:21 +0700

    Complete service and product sale add effects to shopnow button

--- my-app/public/font/slick.swift
+++
@@ -1,4 +0,0 @@
-node_modules/slick-carousel/slick/fonts/slick.eot
-node_modules/slick-carousel/slick/fonts/slick.woff
-node_modules/slick-carousel/slick/fonts/slick.ttf
-node_modules/slick-carousel/slick/fonts/slick.svg
